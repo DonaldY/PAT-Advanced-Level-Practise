8d8948291b0b38c71cb785166a6cef4477202bc6
DonaldY <448641125@qq.com>
2018-01-31 23:51:56 +0800

    day7

---
+++ PAT-Advanced-Level-Practise/1025.cpp
@@ -0,0 +1,23 @@
+/**
+    2018.1.30
+    Donald
+*/
+//1025. PAT Ranking (25)
+
+/**
+
+*/
+
+
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+
+int main(void) {
+
+
+    return 0;
+}
+
