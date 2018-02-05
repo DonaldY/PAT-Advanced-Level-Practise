ff753d7422a530f319960159046e1d3f46e9b946
DonaldY <448641125@qq.com>
2018-02-05 12:32:23 +0800

    update

--- PAT-Advanced-Level-Practise/1029.cpp
+++ PAT-Advanced-Level-Practise/1029.cpp
@@ -12,76 +12,48 @@
 
 
 #include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<algorithm>
-#include<set>
-#include<iostream>
+#include<vector>
 using namespace std;
 #define MAXN 1000005
 
 int N1, N2;
 long seq1[MAXN];
 long seq2[MAXN];
+vector<long> Vec;
 
 int main(void) {
 
     scanf("%d", &N1);
-    for (int i = 1; i <= N1; ++i) {
-        scanf("%lld", &seq1[i]);
+    for (int i = 0; i < N1; ++i) {
+        scanf("%ld", &seq1[i]);
     }
 
     scanf("%d", &N2);
-    for (int i = 1; i <= N2; ++i) {
-        scanf("%lld", &seq2[i]);
+    for (int i = 0; i < N2; ++i) {
+        scanf("%ld", &seq2[i]);
     }
 
-    int seqMedianIndex1 = ceil(N1 * 1.0 / 2);
-    int seqMedianIndex2 = ceil(N2 * 1.0 / 2);
-
-    long seqMedianNum1 = seq1[seqMedianIndex1];
-    long seqMedianNum2 = seq2[seqMedianIndex2];
-
-    long *Seq1 = seq1;
-    long *Seq2 = seq2;
-
-    if (seqMedianNum1 > seqMedianNum2) {
-        long *tempSeq = Seq1;
-        Seq1 = seq2;
-        Seq2 = tempSeq;
-        int temp = N1;
-        N1 = N2;
-        N2 = temp;
-        temp = seqMedianNum1;
-        seqMedianNum1 = seqMedianNum2;
-        seqMedianNum2 = temp;
-
-    } else if (seqMedianNum1 == seqMedianNum2) {
-        printf("%ld", seqMedianNum1);
-        return 0;
-    }
-
-    set<long> Set;
-    for (int i = 1; i <= N1; ++i) {
-        if (Seq1[i] < seqMedianNum2 && Seq1[i] >= seqMedianNum1) {
-            Set.insert(Seq1[i]);
+    int i = 0, j = 0, k = 0;
+    while ( i < N1 && j < N2) {
+        if (seq1[i]  == seq2[j]) {
+            Vec.push_back(seq1[i++]);
+            Vec.push_back(seq2[j++]);
+        } else if (seq1[i] < seq2[j]) {
+            Vec.push_back(seq1[i++]);
+        } else {
+            Vec.push_back(seq2[j++]);
         }
     }
 
-    for (int i = 1; i <= N2; ++i) {
-        if (Seq2[i] < seqMedianNum2 && Seq2[i] >= seqMedianNum1) {
-            Set.insert(Seq2[i]);
-        }
+    while (i < N1) {
+        Vec.push_back(seq1[i++]);
     }
 
-    int index = ceil(Set.size() * 1.0 / 2);
-
-    int cnt = 1;
-    for (set<long>::iterator it = Set.begin(); it != Set.end(); ++it) {
-        if (cnt++ == index) {
-            printf("%ld", *it);
-        }
+    while (j < N2) {
+        Vec.push_back(seq2[j++]);
     }
+
+    printf("%ld", Vec[(Vec.size() - 1) / 2]);
 
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+PAT (Advanced Level Practise) 
+
+Practising and saving source code.
+
+## 分类
+
+### 最短路
+1. 1030
