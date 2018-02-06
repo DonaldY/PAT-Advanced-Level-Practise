ac8a8c2d3434a829866fdd12a1fd44f8efd66913
DonaldY <448641125@qq.com>
2018-02-06 22:23:33 +0800

    next

---
+++ PAT-Advanced-Level-Practise/1030.cpp
@@ -0,0 +1,36 @@
+/**
+    2018.2.5
+    Donald
+*/
+//1030. Travel Plan (30)
+
+/**
+    找出最短路, 根据distancs
+
+    输出 路径、距离、花费
+
+*/
+
+
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+int N;
+int M;
+int S, D;
+
+
+int main(void) {
+
+    scanf("%d %d %d %d", &N, &M, &S, &D);
+
+    for (int i = 0 ; i < M; ++i) {
+
+    }
+
+
+    return 0;
+}
+
